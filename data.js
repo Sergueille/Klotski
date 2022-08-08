e6a6fcd932b4f07475576c63f2ba0eebd418640a
Sergueille <84402628+Sergueille@users.noreply.github.com>
2022-08-08 08:40:07 +0200

    New puzzle!

--- data.js
+++ data.js
@@ -66,5 +66,22 @@ var data = [
         ],
         gameData: null,
         difficulty: "Very hard"
+    },
+    {
+        name: "Super Compo",
+        tiles: [
+            new Block(new vec2(1, 0), new vec2(2), true),
+            new Block(new vec2(1, 2), new vec2(2, 1)),
+            new Block(new vec2(1, 3), new vec2(2, 1)),
+            new Block(new vec2(1, 4), new vec2(2, 1)),
+            new Block(new vec2(0, 1), new vec2(1)),
+            new Block(new vec2(3, 1), new vec2(1)),
+            new Block(new vec2(0, 2), new vec2(1, 2)),
+            new Block(new vec2(3, 2), new vec2(1, 2)),
+            new Block(new vec2(0, 4), new vec2(1)),
+            new Block(new vec2(3, 4), new vec2(1)),
+        ],
+        gameData: null,
+        difficulty: "Very hard"
     }
 ];
